f70879b69fc2cca9c7f92c2590ca08ba34890d32
Mira Estil <mira@Miras-MacBook-Pro.local>
2018-01-31 16:36:43 -0500

    basic cell layout

--- Miyu/Feature/Homepage/HomepageCell/HomepageTableViewCell.swift
+++ Miyu/Feature/Homepage/HomepageCell/HomepageTableViewCell.swift
@@ -10,11 +10,24 @@ import UIKit
 
 class HomepageTableViewCell: UITableViewCell {
 
+    @IBOutlet weak var profileImage: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var ratingLabel: UILabel!
+    
+    @IBOutlet weak var captionLabel: UILabel!
+    
+    // TODO: this will change if video..
+    @IBOutlet weak var contentImage: UIImageView!
+    @IBOutlet weak var commentCaptionLabel: UILabel!
+    @IBOutlet weak var commentCountLabel: UILabel!
+    
+    // TODO: Figure out model to obtain this data.
+    @IBOutlet weak var recentCommentLabel: UILabel!
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         
     }
-
 
     
 }
