ce59b1cc48e1e3685c2eef6e08d6dbbd15d21385
Mira Estil <mira@Miras-MacBook-Pro.local>
2018-02-01 21:27:39 -0500

    update podfile

---
+++ Miyu/Feature/Profile/ProfileViewController.swift
@@ -0,0 +1,38 @@
+//
+//  ProfileViewController.swift
+//  Simi
+//
+//  Created by Mira Estil on 1/31/18.
+//  Copyright © 2018 ME. All rights reserved.
+//
+
+import UIKit
+
+class ProfileViewController: UIViewController {
+
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        setup()
+    }
+    
+    private func setup() {
+        tableView.register(UINib(nibName: Constants.profileXib, bundle: nil),
+                           forCellReuseIdentifier: Constants.profileCell)
+    }
+}
+
+extension ProfileViewController: UITableViewDelegate, UITableViewDataSource {
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: Constants.profileCell, for: indexPath) as! ProfileTableViewCell
+        
+        return cell
+    }
+}
---
+++ Podfile
@@ -0,0 +1,11 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'Simi' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for Simi
+pod 'Cosmos', '~> 14.0'
+
+end
