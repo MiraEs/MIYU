7105e681f7e8941060cec7fc3b821642aa78f8db
Mira Estil <mira@Miras-MacBook-Pro.local>
2018-02-03 00:28:00 -0500

    edit skeleton upload feat

--- Miyu/Feature/Upload/UploadViewController.swift
+++ Miyu/Feature/Upload/UploadViewController.swift
@@ -10,26 +10,22 @@ import UIKit
 
 class UploadViewController: UIViewController {
 
+    @IBOutlet weak var profileImage: UIImageView!
+    @IBOutlet weak var name: UILabel!
+    @IBOutlet weak var rating: UILabel!
+    
+    
+    @IBOutlet weak var captionTextView: UITextView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func editCaptionTapped(_ sender: Any) {
+        print("edit caption tapped")
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    @IBAction func uploadImageTapped(_ sender: Any) {
+        print("upload Image tapped")
     }
-    */
-
 }
