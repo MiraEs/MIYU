4b36d02a14ef5910c509958345231de4028ba47c
Mira Estil <mira@Miras-MacBook-Pro.local>
2018-02-09 21:54:02 -0500

    refactored uploadViewModel

--- Miyu/Feature/Upload/UploadViewController.swift
+++ Miyu/Feature/Upload/UploadViewController.swift
@@ -17,6 +17,10 @@ class UploadViewController: UIViewController {
     @IBOutlet weak var captionTextView: UITextView!
     @IBOutlet weak var centerImage: UIButton!
     
+    private var viewModel: UploadViewModel! {
+        return UploadViewModel(presentVc: self)
+    }
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -29,71 +33,12 @@ class UploadViewController: UIViewController {
     
     @IBAction func uploadContent(_ sender: Any) {
         print("upload pic to storage")
-        uploadPic()
+        viewModel?.uploadPic(with: profileImage)
     }
     
     @IBAction func uploadImageTapped(_ sender: Any) {
         print("upload Image tapped")
-        showPicker()
-    }
-    
-    // MARK: FIREBASE STORAGE
-    
-    private func uploadPic() {
-        // current user uid will make key
-        guard let key = Auth.auth().currentUser?.uid else { return }
-        let imageName = NSUUID().uuidString
-        let storageRefImages = Storage.storage().reference().child("images").child(key).child("\(imageName).png")
-        //let storageRefVideos = Storage.storage().reference().child("videos").child(key).child("imageName")
-        let userStorageRef = Storage.storage().reference().child("users").child(key).child("\(imageName).png")
-        let storageRef = Storage.storage().reference().child("allContent").child("\(imageName).png")
-        if let image = profileImage.image {
-            let uploadData = UIImagePNGRepresentation(image)
-            
-            // Ref 1 - Images/user
-            storageRefImages.putData(uploadData!, metadata: nil, completion: { (metadata, error) in
-                if error != nil {
-                    print(error?.localizedDescription)
-                }
-                if let urlString = metadata?.downloadURL()?.absoluteString {
-                    self.uploadToDatabase(urlString)
-                }
-            })
-            
-            // Ref 2 - User
-            userStorageRef.putData(uploadData!, metadata: nil, completion: nil)
-            
-            // Ref 3 - Url/Userkey - all content with user key
-            storageRef.putData(uploadData!, metadata: nil, completion: { (nil, error) in
-                if error != nil {
-                    print(error?.localizedDescription)
-                }
-            })
-        }
-    }
-    
-    private func uploadToDatabase(_ contentUrl: String) {
-        guard let uid = Auth.auth().currentUser?.uid else { return }
-        
-        // Currently just updating to "posts"
-        let ref = Database.database().reference()
-        let key = ref.child("posts").childByAutoId().key
-        let post: [String: Any] = [
-            "caption": "caption1",
-            "data" : contentUrl
-        ]
-        
-        let childUpdates = ["/posts/\(key)": post,
-                            "/user-posts/\(uid)/\(key)/": post]
-        ref.updateChildValues(childUpdates)
-        print("update to database")
-    }
-    
-    private func showPicker() {
-        let picker = UIImagePickerController()
-        picker.delegate = self
-        picker.allowsEditing = true
-        present(picker, animated: true, completion: nil)
+        viewModel?.showPicker()
     }
     
     
@@ -117,7 +62,5 @@ extension UploadViewController: UIImagePickerControllerDelegate, UINavigationCon
         
         dismiss(animated: true, completion: nil)
     }
-    
-    
 }
 
---
+++ Miyu/Feature/Upload/UploadViewModel.swift
@@ -0,0 +1,79 @@
+//
+//  UploadViewModel.swift
+//  Miyu
+//
+//  Created by Mira Estil on 2/9/18.
+//  Copyright © 2018 ME. All rights reserved.
+//
+
+import UIKit
+import Firebase
+
+internal final class UploadViewModel {
+    
+    var presentingViewController: UIViewController?
+    private var fbManager = FirebaseUserManager.manager
+    
+    init(presentVc: UIViewController) {
+        self.presentingViewController = presentVc
+    }
+    
+    func showPicker() {
+        let picker = UIImagePickerController()
+        guard let presentingView = self.presentingViewController else { return }
+        picker.delegate = presentingView as? UIImagePickerControllerDelegate & UINavigationControllerDelegate
+        picker.allowsEditing = true
+        presentingViewController?.present(picker, animated: true, completion: nil)
+    }
+    
+    func uploadPic(with profileImage: UIImageView) {
+        // current user uid will make key
+        guard let key = Auth.auth().currentUser?.uid else { return }
+        let imageName = NSUUID().uuidString
+        let storageRefImages = Storage.storage().reference().child("images").child(key).child("\(imageName).png")
+        //let storageRefVideos = Storage.storage().reference().child("videos").child(key).child("imageName")
+        let userStorageRef = Storage.storage().reference().child("users").child(key).child("\(imageName).png")
+        let storageRef = Storage.storage().reference().child("allContent").child("\(imageName).png")
+        if let image = profileImage.image {
+            let uploadData = UIImagePNGRepresentation(image)
+            
+            // Ref 1 - Images/user
+            storageRefImages.putData(uploadData!, metadata: nil, completion: { (metadata, error) in
+                if error != nil {
+                    print(error!)
+                }
+                if let urlString = metadata?.downloadURL()?.absoluteString {
+                    self.uploadToDatabase(urlString)
+                }
+            })
+            
+            // Ref 2 - User
+            userStorageRef.putData(uploadData!, metadata: nil, completion: nil)
+            
+            // Ref 3 - Url/Userkey - all content with user key
+            storageRef.putData(uploadData!, metadata: nil, completion: { (nil, error) in
+                if error != nil {
+                    print(error!)
+                }
+            })
+        }
+    }
+    
+    private func uploadToDatabase(_ contentUrl: String) {
+        guard let uid = Auth.auth().currentUser?.uid else { return }
+        
+        // Currently just updating to "posts"
+        let ref = Database.database().reference()
+        let key = ref.child("posts").childByAutoId().key
+        let post: [String: Any] = [
+            "caption": "caption1",
+            "data" : contentUrl
+        ]
+        
+        let childUpdates = ["/posts/\(key)": post,
+                            "/user-posts/\(uid)/\(key)/": post]
+        ref.updateChildValues(childUpdates)
+        print("update to database")
+    }
+
+}
